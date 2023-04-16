43f93e62158e26bbc49fe888d68663a273428b39
Nikita Turabov <turabov@jivosite.com>
2023-04-16 23:30:45 +0300

    подредактировал шапку, отрефакторил FlexContainer, добавил Logo

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,3 +1,3 @@
 export const Home = () => {
-  return <>{'ка дела'}</>;
+  return <div></div>;
 };
--- src/components/Flex/FlexContainer/index.tsx
+++ src/components/Flex/FlexContainer/index.tsx
@@ -1,34 +1,25 @@
 import {FunctionComponent, PropsWithChildren} from 'react';
+import cn from 'classnames';
 
 type TFlexContainer = {
-  direction: 'column' | 'row';
+  direction?: 'column' | 'row';
   classNames?: string;
-  justify: 'between' | 'center' | 'start' | 'end' | 'around' | 'evenly' | 'stretch' | 'baseline';
-  alignContent: 'between' | 'center' | 'start' | 'end' | 'around' | 'evenly' | 'stretch' | 'baseline';
-  wrap: 'wrap' | 'nowrap' | 'wrap-reverse';
-  gap: number;
+  justify?: 'between' | 'center' | 'start' | 'end' | 'around' | 'evenly' | 'stretch' | 'baseline';
+  alignContent?: 'between' | 'center' | 'start' | 'end' | 'around' | 'evenly' | 'stretch' | 'baseline';
+  wrap?: 'wrap' | 'nowrap' | 'wrap-reverse';
+  gap?: number;
 }
 
 export const FlexContainer: FunctionComponent<PropsWithChildren<TFlexContainer>> = (props) => {
-  const {
-    classNames,
-    direction = 'row',
-    justify = 'center',
-    alignContent = 'center',
-    wrap = 'wrap',
-    gap = 2,
-    children
-  } = props;
+  const {classNames, direction, justify, alignContent, wrap, gap, children} = props;
 
   return <div
-    className={`flex 
-      flex-${wrap}
-      flex-${direction}
-      basis-full
-      justify-${justify} 
-      content-${alignContent} 
-      gap-${gap} 
-      ${classNames}`}
+    className={cn('flex', wrap && `flex-${wrap}`,
+      direction && `flex-${direction}`,
+      justify && `justify-${justify}`,
+      alignContent && `content-${alignContent}`,
+      gap && `gap-x-${gap}`,
+      classNames || '')}
   >
     {children}
   </div>;
