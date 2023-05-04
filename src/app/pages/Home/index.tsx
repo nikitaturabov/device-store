82e4a10917cc46228786e63e6d7ffb8ba949bf9a
Nikita Turabov <turabov@jivosite.com>
2023-05-05 00:02:52 +0300

    поправил баги в Typography компонентах

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -7,20 +7,19 @@ export const Home = () => {
   return <FlexContainer className={'h-full'} justify={'center'}>
     <FlexItem>
       <form action=''>
-        <FlexContainer direction={'column'} justify={'center'} gap={2}>
-          <Title.H4>{'Окошечко авторизации'}</Title.H4>
-          <FlexItem>
-            <FlexContainer direction={'row'} alignItems={'center'} justify={'center'} gap={2}>
-              <Text.Small>{'Login'}</Text.Small>
-              <input type={'text'} value={'admin'} className={'w-full rounded-md border border-[#DDE2E4] px-3 py-2 text-sm'} />
-            </FlexContainer>
-          </FlexItem>
-          <FlexItem>
-            <FlexContainer direction={'row'} alignItems={'center'} justify={'center'} gap={2}>
-              <Text.Small>{'Password'}</Text.Small>
-              <input type={'password'} value={'admin'} className={'w-full rounded-md border border-[#DDE2E4] px-3 py-2 text-sm'} />
-            </FlexContainer>
-          </FlexItem>
+        <FlexContainer direction={'column'} justify={'center'} alignItems={'center'} gap={4}>
+          <Title.H4 align={'center'}>{'Окошечко авторизации'}</Title.H4>
+          <FlexContainer direction={'row'} alignItems={'center'} justify={'between'} gap={2}>
+            <Text.Small className={'w-32'}>{'Login'}</Text.Small>
+            <input type={'text'} value={'admin'} className={'w-full rounded-md border border-[#DDE2E4] px-3 py-2 text-sm'} />
+          </FlexContainer>
+
+
+          <FlexContainer direction={'row'} alignItems={'center'} justify={'between'} gap={2}>
+            <Text.Small className={'w-32'}>{'Password'}</Text.Small>
+            <input type={'password'} value={'admin'} className={'w-full rounded-md border border-[#DDE2E4] px-3 py-2 text-sm'} />
+          </FlexContainer>
+
         </FlexContainer>
       </form>
     </FlexItem>
--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -9,57 +9,57 @@ const getGeneralClasses = ({weight, align, color}: TClassesArgs) => cn('leading-
   [`text-${color}`]: color
 });
 export const H1: FunctionComponent<TTitleProps> = (props) => {
-  const {weight, align, color, classNames, children} = props;
+  const {weight, align, color, className, children} = props;
 
-  return <h1 className={cn('text-4xl', classNames,
+  return <h1 className={cn('text-4xl', className,
     getGeneralClasses({weight, align, color})
   )}>{children}</h1>;
 };
 
 export const H2: FunctionComponent<TTitleProps> = (props) => {
-  const {weight, align, color, classNames, children} = props;
+  const {weight, align, color, className, children} = props;
 
-  return <h2 className={cn('text-3xl', classNames,
+  return <h2 className={cn('text-3xl', className,
     getGeneralClasses({weight, align, color})
   )}>{children}</h2>;
 };
 
 export const H3: FunctionComponent<TTitleProps> = (props) => {
-  const {weight, align, color, classNames, children} = props;
+  const {weight, align, color, className, children} = props;
 
-  return <h3 className={cn('text-2xl', classNames,
+  return <h3 className={cn('text-2xl', className,
     getGeneralClasses({weight, align, color})
   )}>{children}</h3>;
 };
 
 export const H4: FunctionComponent<TTitleProps> = (props) => {
-  const {weight, align, color, classNames, children} = props;
+  const {weight, align, color, className, children} = props;
 
-  return <h4 className={cn('text-xl', classNames,
+  return <h4 className={cn('text-xl', className,
     getGeneralClasses({weight, align, color})
   )}>{children}</h4>;
 };
 
 export const Large: FunctionComponent<TTextProps> = (props) => {
-  const {weight, align, color, classNames, children} = props;
+  const {weight, align, color, className, children} = props;
 
-  return <div className={cn('text-lg', classNames,
+  return <div className={cn('text-lg', className,
     getGeneralClasses({weight, align, color})
   )}>{children}</div>;
 };
 
 export const Base: FunctionComponent<TTextProps> = (props) => {
-  const {weight, align, color, classNames, children} = props;
+  const {weight, align, color, className, children} = props;
 
-  return <div className={cn('text-base', classNames,
+  return <div className={cn('text-base', className,
     getGeneralClasses({weight, align, color})
   )}>{children}</div>;
 };
 
 export const Small: FunctionComponent<TTextProps> = (props) => {
-  const {weight, align, color, classNames, children} = props;
+  const {weight, align, color, className, children} = props;
 
-  return <div className={cn('text-sm', classNames,
+  return <div className={cn('text-sm', className,
     getGeneralClasses({weight, align, color})
   )}>{children}</div>;
 };
